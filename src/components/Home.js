6ee35cc4e16c24c4104ef6f95d797a4c4ff181bf
Aaron Melton <amelton@Aarons-MacBook-Pro.local>
2024-07-11 19:02:47 -0400

    Fix ESLint warnings and add missing Babel plugin

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function Home() {

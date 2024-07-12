98f8de6b3ba97079616dbe7c9736e3d75331b63a
Aaron Melton <amelton@Aarons-MacBook-Pro.local>
2024-07-12 19:12:06 -0400

    Add PrivacyPolicy component

---
+++ src/components/PrivacyPolicy.js
@@ -0,0 +1,12 @@
+// src/components/PrivacyPolicy.js
+
+import React from 'react';
+
+const PrivacyPolicy = () => (
+  <div>
+    <h1>Privacy Policy</h1>
+    <p>Your privacy policy content goes here.</p>
+  </div>
+);
+
+export default PrivacyPolicy;

b5e32b72e1b9691e04d78ca8f8c351288a376890
Aaron Melton <amelton@Aarons-MacBook-Pro.local>
2024-07-12 17:55:18 -0400

    change ui for mobile

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -15,7 +15,7 @@ function Chat() {
 
   const initGoogleSignIn = useCallback(() => {
     window.google.accounts.id.initialize({
-      client_id: '88973414867-h7amkrgb8s3onoopm4a3jaaddtjoefas.apps.googleusercontent.com', // Replace with your actual Client ID
+      client_id: '88973414867-h7amkrgb8s3onoopm4a3jaaddtjoefas.apps.googleusercontent.com',
       callback: async (response) => {
         if (response.credential) {
           const payload = parseJwt(response.credential);
@@ -270,11 +270,12 @@ function Chat() {
     padding: '5px 15px',
     borderRadius: '5px',
     cursor: 'pointer',
-    marginLeft: 'auto',
     fontSize: '14px',
     fontWeight: 'bold',
     border: 'none',
-    marginTop: '10px',
+    position: 'fixed',
+    top: '10px',
+    right: '10px',
   };
 
   const markdownOptions = {

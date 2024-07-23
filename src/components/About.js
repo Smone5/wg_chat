e64ea592008adefd7a8867e625c16e1593a045eb
Aaron Melton <amelton@Aarons-MacBook-Pro.local>
2024-07-23 14:30:29 -0400

    added about page

--- src/components/About.js
+++ src/components/About.js
@@ -11,13 +11,25 @@ function About() {
           </header>
           <section>
             <p className="text-muted mb-3">
-              The Unofficial Walgreens Chatbot is designed to help you with your Walgreens-related inquiries. Whether you need assistance with pharmacy services, customer service, or any other Walgreens-related information, our chatbot is here to help.
+              Hi, I'm Aaron Melton, the creator of the Unofficial Walgreens Chatbot. 
+              As a former Senior Natural Language Understanding Analyst at Walgreens, I was 
+              dedicated to improving our chatbot's machine learning model. However, 
+              due to contract constraints and leadership issues, I found it challenging 
+              to make the necessary improvements.
             </p>
             <p className="text-muted mb-3">
-              This chatbot is currently in Beta. We appreciate your feedback and suggestions to improve its performance and functionality.
+              I eventually resigned from Walgreens because I was frustrated with the inaccuracies in reporting a systematic problem. 
+              Feeling embarrassed and constrained by the limitations, I decided to create a new chatbot as a part of my job portfolio. 
+              This project allowed me to implement the improvements I envisioned and showcase my skills and dedication 
+              to enhancing user experiences through advanced machine learning techniques.
             </p>
             <p className="text-muted mb-3">
-              Our goal is to provide a seamless and efficient experience for Walgreens customers. We use your Google profile ID to create personalized chatbot sessions, ensuring the chatbot understands your previous conversations and can provide more accurate assistance.
+              This chatbot has a React front end and Langchain backend. It uses Retrieval Augmented Generation (RAG) to look up information and prevent hallucinations. In the future, I may add functions like finding the nearest store, deals/coupons, and other great ideas people suggest. It was designed to be a prototype and scalable.
+            </p>
+            <p className="text-muted mb-3">
+              Thank you for trying out the Unofficial Walgreens Chatbot. I hope it 
+              provides you with the assistance you need and demonstrates the potential 
+              of what a well-tuned chatbot can achieve.
             </p>
           </section>
         </div>

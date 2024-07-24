3eb555189f2ec4f3875f2a87c90127f329e55bb7
Aaron Melton <amelton@Aarons-MacBook-Pro.local>
2024-07-23 21:47:19 -0400

    updated css amd chat.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -12,9 +12,10 @@ function Layout({ children }) {
           <Link className="navbar-brand d-flex align-items-center" to="/">
             <span className="brand-text">Independent Walgreens Expertise</span>
             <img
-              src="https://wgchat.blob.core.windows.net/images/android-chrome-192x192.png" // Replace with your icon URL
+              src="https://wgchat.blob.core.windows.net/images/android-chrome-192x192.png"
               alt="icon"
               className="brand-icon"
+              style={{ width: '30px', height: '30px', marginLeft: '0.5rem' }}
             />
           </Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
